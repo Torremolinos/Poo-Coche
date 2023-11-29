74e3a717615e2108ea4170db940fb333564500e5
Torremolinos <adrian_2054@hotmail.com>
2023-11-29 09:03:30 +0100

    Acabamos la clase vehiculo

--- scripts/vehiculo.js
+++ scripts/vehiculo.js
@@ -1,5 +1,5 @@
 class Vehiculo {
-    #marca; //private ##.
+    #marca; //private #.
     #modelo;
     #color;
     #fabricacion;
@@ -26,5 +26,65 @@ class Vehiculo {
         Se fabricó el año -> ${this.#fabricacion}
         <br/>
         Cilindrada -> ${this.#cilindrada}`;
+
+        //llamar a la funcion mostrarREsultado definida en el main.js
+        mostrarResultado(`<p>${datos}</p>`);
+    }
+
+    acelerar(velocidad) {
+        const mensaje = `<br/> Se ha celerado hasta -> ${velocidad}`;
+        mostrarResultado(`<p>${datos}</p>`);
+    }
+    //aqui llamamos a los atributos con el operador this. porque es private.
+    arrancar() {
+        const mensaje = `<br/> El coche${this.#marca}, ${this.#modelo}, de color ${this.color} ha arrancado.`;
+        mostrarResultado(`<p>${datos}</p>`);
+    }
+
+    frenar(){
+        const mensaje = `<br/> El coche${this.#marca}, ${this.#modelo}, de color ${this.color} ha parado.`;
+        mostrarResultado(`<p>${datos}</p>`);
+    }
+
+    get marca(){
+        return this.#marca;
+    }
+    
+    set marca(value){
+        this.#marca = value;
+    }
+
+    get modelo(){
+        return this.#modelo;
+    }
+ 
+    set modelo(modelo){
+        this.#modelo = modelo;
+   
+    }
+ 
+    get color(){
+        return this.#color;
+    }
+ 
+    set color(color){
+        this.#color = color;
+   
+    }
+ 
+    get fabricacion(){
+        return this.#fabricacion;
+    }
+ 
+    set fabricacion(fabricacion){
+        this.#fabricacion = fabricacion;
+    }
+ 
+    get cilindrada() {
+        return this.#cilindrada;
+    }
+ 
+    set cilindrada(cilindrada) {
+        this.#cilindrada = cilindrada;
     }
 }
